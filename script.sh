ce95b9a3a3bda5d6a5b4e8b7e6daf4e08b27d744
Josh L <joshua.lamb@prts.edu>
2018-12-07 21:39:30 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,19 +3,25 @@ set -e
 
 if [ -z "$PLUGIN_NETLIFY_AUTH_TOKEN" ]
 then
-    echo "> Error! NETLIFY_AUTH_TOKEN not defined"
+    echo "> Error! netlify_auth_token not defined"
     exit 1
 fi
 
 if [ -z "$PLUGIN_NETLIFY_SITE_ID" ]
 then
-    echo "> Error! NETLIFY_SITE_ID not defined"
+    echo "> Error! netlify_site_id not defined"
+    exit 1
+fi
+
+if [ -z "$PLUGIN_DIR" ]
+then
+    echo "> Error! dir not defined"
     exit 1
 fi
 
 if [ -n "$PLUGIN_MESSAGE" ]
 then
-    netlify deploy --auth $PLUGIN_NETLIFY_AUTH_TOKEN --site $PLUGIN_NETLIFY_SITE_ID --message $PLUGIN_MESSAGE
+    netlify deploy --auth $PLUGIN_NETLIFY_AUTH_TOKEN --site $PLUGIN_NETLIFY_SITE_ID --dir $PLUGIN_DIR --message $PLUGIN_MESSAGE
 else
-    netlify deploy --auth $PLUGIN_NETLIFY_AUTH_TOKEN --site $PLUGIN_NETLIFY_SITE_ID
+    netlify deploy --auth $PLUGIN_NETLIFY_AUTH_TOKEN --site $PLUGIN_NETLIFY_SITE_ID --dir $PLUGIN_DIR
 fi
