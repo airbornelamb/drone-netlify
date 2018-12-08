044f98d7825ba78f18f45672c2d470eb39b424c8
Josh L <joshua.lamb@prts.edu>
2018-12-08 00:25:35 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,27 +1,30 @@
 #!/bin/sh
 set -e
 
-if [ -z "$PLUGIN_NETLIFY_AUTH_TOKEN" ]
-then
+PROD=""
+
+if [ -z "$PLUGIN_NETLIFY_AUTH_TOKEN" ] ; then
     echo "> Error! netlify_auth_token not defined"
     exit 1
 fi
 
-if [ -z "$PLUGIN_NETLIFY_SITE_ID" ]
-then
+if [ -z "$PLUGIN_NETLIFY_SITE_ID" ] ; then
     echo "> Error! netlify_site_id not defined"
     exit 1
 fi
 
-if [ -z "$PLUGIN_DIR" ]
-then
+if [ -z "$PLUGIN_DIR" ] ; then
     echo "> Error! dir not defined"
     exit 1
 fi
 
-if [ -n "$PLUGIN_MESSAGE" ]
-then
-    netlify deploy --auth $PLUGIN_NETLIFY_AUTH_TOKEN --site $PLUGIN_NETLIFY_SITE_ID --dir $PLUGIN_DIR --message $PLUGIN_MESSAGE
+if [ "$PLUGIN_PRODUCTION" = "yes" ] ; then
+    PROD="--prod"
+fi
+    
+
+if [ -n "$PLUGIN_MESSAGE" ] ; then
+    netlify deploy --auth $PLUGIN_NETLIFY_AUTH_TOKEN --site $PLUGIN_NETLIFY_SITE_ID --dir $PLUGIN_DIR --message $PLUGIN_MESSAGE $PROD
 else
-    netlify deploy --auth $PLUGIN_NETLIFY_AUTH_TOKEN --site $PLUGIN_NETLIFY_SITE_ID --dir $PLUGIN_DIR
+    netlify deploy --auth $PLUGIN_NETLIFY_AUTH_TOKEN --site $PLUGIN_NETLIFY_SITE_ID --dir $PLUGIN_DIR $PROD
 fi
